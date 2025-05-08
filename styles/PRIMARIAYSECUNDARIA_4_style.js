a76192845d6c672abde06a5cfe091fabf0db3004
INYGEO <inygeo@hotmail.com>
2025-05-08 11:55:57 -0700

    Add files via upload

--- styles/PRIMARIAYSECUNDARIA_4_style.js
+++ styles/PRIMARIAYSECUNDARIA_4_style.js
@@ -6,7 +6,7 @@ function categories_PRIMARIAYSECUNDARIA_4(feature, value, size, resolution, labe
                 var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
                 switch(valueStr) {case 'PRIMARIA':
                     return [ new ol.style.Style({
-        image: new ol.style.RegularShape({radius: 4.0 + size, points: 4,
+        image: new ol.style.RegularShape({radius: 6.0 + size, points: 4,
             angle: Math.PI/4, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(86,237,75,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -15,7 +15,7 @@ function categories_PRIMARIAYSECUNDARIA_4(feature, value, size, resolution, labe
                     break;
 case 'SECUNDARIA':
                     return [ new ol.style.Style({
-        image: new ol.style.RegularShape({radius: 4.0 + size, points: 3,
+        image: new ol.style.RegularShape({radius: 6.0 + size, points: 3,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(193,53,35,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
